cb07e1b8f40b962f4b0b643f9f7d2a940cd89ddf
Rinat <polaroid264@yandex.ru>
2025-06-11 14:06:54 +0300

    Реализация клиента через RestClient

--- stat/stat-client/src/main/java/StatClient.java
+++ stat/stat-client/src/main/java/StatClient.java
@@ -1,9 +1,52 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
-@SpringBootApplication
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@Slf4j
+@Service
 public class StatClient {
-    public static void main(String[] args) {
-        SpringApplication.run(StatClient.class, args);
+    private final RestClient client;
+
+    public StatClient(@Value("${stat-server-url}") String url) {
+        this.client = RestClient.builder()
+                .baseUrl(url)
+                .build();
+    }
+
+    public void sendHit(RequestHitDto hit) {
+        try {
+            client.post()
+                    .uri("/hit")
+                    .contentType(APPLICATION_JSON)
+                    .body(hit).retrieve()
+                    .toBodilessEntity();
+        } catch (Exception e) {
+            log.info("Ошибка при записи статистики: {}", e.getMessage());
+        }
+    }
+
+    public List<ResponseDto> getStats(String start, String end, List<String> uris, boolean unique) {
+        try {
+            client.get()
+                    .uri(uriBuilder -> uriBuilder.path("/stats")
+                            .queryParam("start", start)
+                            .queryParam("end", end)
+                            .queryParam("uris", uris)
+                            .queryParam("unique", unique)
+                            .build())
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<List<ResponseDto>>() {
+                    });
+        } catch (Exception e) {
+            log.info("Ошибка при выводе статистики: {}", e.getMessage());
+        }
+        return Collections.emptyList();
     }
 }
